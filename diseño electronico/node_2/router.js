97f16b2e408c866bbf2f772e99c704c15585b5c6
Felipe Díaz <47798722+afelipe00@users.noreply.github.com>
2020-10-09 16:12:05 -0500

    feat: actualizacion codigo para manejar con la ESP32

--- diseño electronico/node_2/router.js
+++ diseño electronico/node_2/router.js
@@ -1,11 +1,37 @@
 const express =  require("express")
 const router = express.Router()
 
-
-router.get('/saludo', (re,res, next) => {
-    console.log('hicieron un res a saludo')
-    res.send('esta es la respuesta de saludo')
-
+router.get('/saludo/:nombre/:apellido', (req,res, next) => {
+    console.log('hicieron un req a saludo');
+    var payload = {
+        'id': 1,
+        'mensaje' : 'Hola ' + req.params.nombre + ' ' + req.params.apellido + ' bienvenido a saludo'
+    }
+    res.send(payload);
 })
+
+router.get('/hora', (req,res, next)=>{
+    console.log('hicieron req a hora');
+    let payload = {
+        'id': 2,
+        'hora': getDatetime()
+    }
+    res.send(payload)
+})
+
+router.get('/temperatura/:temp', (req,res, next)=>{
+    console.log('hicieron req a temperatura');
+    console.log(req.params)
+    let payload = {
+        'id': 3,
+        'temperatura state': 'temperatura ok' 
+    }
+    res.send(payload)
+})
+
+function getDatetime(){
+    let date = new Date();
+    return date;
+}
 
 module.exports = router;
