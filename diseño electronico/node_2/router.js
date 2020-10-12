9ce9b44c5df5d171a00cb9fe371d00ec76bf1387
Felipe Díaz <47798722+afelipe00@users.noreply.github.com>
2020-10-12 01:51:52 -0500

    feat: :sparkles: se agrego la opcion de registrar usuario

--- diseño electronico/node_2/router.js
+++ diseño electronico/node_2/router.js
@@ -1,10 +1,29 @@
 const express =  require("express")
 const router = express.Router()
+var md5 = require('md5');
+const fs = require('fs')
+
+
 
 //declaracion de variables
 let tempArray = [];
+let arrayUsers = [];
 
-//ws de saludo
+//se cargan los datos
+loadData();
+
+function loadData(){
+    fs.readFile('datos.json', (err, data) =>{
+        if(err){
+            console.log(err);
+        }else{
+            arrayUsers = JSON.parse(data)
+            console.log(datar)
+        }
+    })
+}
+
+//ws de saludo GET
 router.get('/saludo/:nombre/:apellido', (req,res, next) => {
     console.log('hicieron un req a saludo');
     var payload = {
@@ -14,7 +33,7 @@ router.get('/saludo/:nombre/:apellido', (req,res, next) => {
     res.send(payload);
 })
 
-//ws de hora
+//ws de hora GET
 router.get('/hora', (req,res, next)=>{
     console.log('hicieron req a hora');
     let payload = {
@@ -24,7 +43,7 @@ router.get('/hora', (req,res, next)=>{
     res.send(payload)
 })
 
-//ws de recivir temperatura
+//ws de recibir temperatura GET
 router.get('/temperaturaIn/:temp', (req,res, next)=>{
     console.log('Req. de temperatura');
     console.log(req.params)
@@ -37,7 +56,7 @@ router.get('/temperaturaIn/:temp', (req,res, next)=>{
         date: getDatetime()
     }
     try{
-        tempArray.push(payload)
+        tempArray.push(Object.assign({},payload))
         message = 'dato almacenado correctamente'
     } catch(error){
         message = error;
@@ -46,13 +65,13 @@ router.get('/temperaturaIn/:temp', (req,res, next)=>{
 })
 
 
-//ws de transmitir temparray
+//ws de transmitir temparray GET
 router.get('/temperaturaOut', (req, res, next)=>{
     console.log('solicitud de array')
     res.send(tempArray);
 })
 
-//ws de borrar array
+//ws de borrar array GET
 router.get('/temperaturaDel', (req, res, next)=>{
     console.log('!!ARRAY DELETE!!')
     try{
@@ -63,7 +82,89 @@ router.get('/temperaturaDel', (req, res, next)=>{
     }
 })
 
-//funcion para obtener la fecha altual.
+//ws tipo post
+router.post('/placa',(req,res,next)=>{
+    
+    console.log(req.body)
+    var payload = {
+        mensaje: 'OK'
+    }
+    res.send(payload)
+});
+
+//ws usuario y contrasenna 
+router.post('/usuarioIn',(req,res,next)=>{
+    var dataUsers = {
+        user: req.body.user,
+        password: md5(req.body.password)
+    };
+    let payload = {
+        message: 'undefined'
+    }
+    if (findUser(dataUsers.user)){
+        user = findUser(dataUsers.user);
+        if (user.password == dataUsers.password){
+            payload.message = 'Bienvenido'
+        }else{
+            payload.message = 'contrasenna incorrecta'
+        }
+    }else{
+        payload.message = 'usuario no existente'
+    }
+    res.send(payload)
+})
+//funcion para encontrar usuario
+function findUser(user){
+    for(dataUser of arrayUsers){
+        if(dataUser.user === user){
+            return dataUser
+        }
+    }
+    return undefined
+}
+
+//ws registrar usuario
+router.post('/userReg', (req, res,next) =>{
+    var dataUser = {
+        name: req.body.name,
+        username: req.body.username,
+        country: req.body.country,
+        password: req.body.password,
+        email: req.body.email
+    }
+    var payload = {
+        message: 'usuario ingresado correctamente'
+    }
+    arrayUsers.push(JSON.parse(JSON.stringify(dataUser)))
+})
+
+//ws eliminar usario
+router.post('/userDel', (req,res,next) =>{
+    var dataUsers = {
+        user: req.body.user,
+        password: md5(req.body.password)
+    };
+    let payload = {
+        message: 'undefined'
+    }
+    if (findUser(dataUsers.user)){
+        user = findUser(dataUsers.user);
+        if (user.password == dataUsers.password){
+            for(var i = 0 ; i < arrayUsers.length ; i++){
+                if (arrayUsers[i].user === user){
+                    arrayUsers.splice(i,1)
+                }
+            }
+            payload.message = 'usuario eliminado'
+        }else{
+            payload.message = 'contrasenna incorrecta'
+        }
+    }else{
+        payload.message = 'usuario no existente'
+    }
+    res.send(payload)
+})
+//funcion para obtener la fecha altual. 
 function getDatetime(){
     let date = new Date();
     return date;
