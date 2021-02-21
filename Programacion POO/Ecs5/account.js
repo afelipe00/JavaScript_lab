a2bedffe322b0d13b396fa27b3363235a9050201
Felipe Díaz <47798722+afelipe00@users.noreply.github.com>
2021-02-20 21:15:33 -0500

    bases js

---
+++ Programacion POO/Ecs5/account.js
@@ -0,0 +1,5 @@
+function Account(name, document) {
+    this.name = name;
+    this.document = document;
+    this.email;
+}
---
+++ Programacion POO/Ecs5/index.js
@@ -0,0 +1,4 @@
+var car = new Car('AW123',new Account('Andres Herrera','1234567'))
+
+car.passenger = '123';
+car.printDataCar();
